cd4d3ec47401ab4fc71167eeed40936c28d6f06d
Sachin Rana <sachinrana1827@gmail.com>
2021-12-09 18:49:53 +0530

    added snake and food

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -4,6 +4,13 @@
 MainComponent::MainComponent()
 {
     setSize (600, 400);
+    setFramesPerSecond(10);
+    setWantsKeyboardFocus(true);
+    direction = Direction::RIGHT;
+    pieces = 20;
+    box = getWidth() / pieces;
+    snake = { juce::Point<int>(1 * box, 1 * box) };
+    food = randomFood();
 }
 
 MainComponent::~MainComponent()
@@ -14,11 +21,14 @@ MainComponent::~MainComponent()
 void MainComponent::paint (juce::Graphics& g)
 {
     // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+    g.fillAll (juce::Colours::white);
 
-    g.setFont (juce::Font (16.0f));
-    g.setColour (juce::Colours::white);
-    g.drawText ("Hello World!", getLocalBounds(), juce::Justification::centred, true);
+    g.setColour(juce::Colours::red);
+    for (const auto &part: snake) {
+        g.fillRect(part.getX(), part.getY(), box, box);
+    }
+    g.setColour(juce::Colours::green);
+    g.fillRect(food.getX(), food.getY(), box, box);
 }
 
 void MainComponent::resized()
@@ -26,4 +36,22 @@ void MainComponent::resized()
     // This is called when the MainComponent is resized.
     // If you add any child components, this is where you should
     // update their positions.
+}
+
+void MainComponent::update() {
+    // old head position
+    auto oldHead{ snake[0] };
+    if (direction == Direction::LEFT) oldHead.x -= box;
+    else if (direction == Direction::RIGHT) oldHead.x += box;
+    else if (direction == Direction::UP) oldHead.y -= box;
+    else oldHead.y += box;
+
+    if (oldHead.x == food.x && oldHead.y == food.y) {
+        score++;
+        food = randomFood();
+    }
+    else snake.pop_back();
+
+    // add new head
+    snake.insert(snake.begin(), oldHead);
 }
--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -7,7 +7,7 @@
     This component lives inside our window, and this is where you should put all
     your controls and content.
 */
-class MainComponent  : public juce::Component
+class MainComponent  : public juce::AnimatedAppComponent
 {
 public:
     //==============================================================================
@@ -17,11 +17,24 @@ public:
     //==============================================================================
     void paint (juce::Graphics&) override;
     void resized() override;
-
+    void update() override;
+    juce::Point<int> randomFood() {
+        return juce::Point<int>(random.nextInt(pieces - 3) * box + box, random.nextInt(pieces - 3) * box + box);
+    }
+    //bool keyPressed(const juce::KeyPress &, Component*) override;
 private:
     //==============================================================================
     // Your private member variables go here...
-
+    enum class Direction {
+        UP, DOWN, LEFT, RIGHT
+    };
+    Direction direction;
+    juce::Random random;
+    int pieces;
+    int box;
+    std::vector<juce::Point<int>> snake;
+    juce::Point<int> food;
+    int score;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
 };
